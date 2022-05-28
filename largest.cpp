107db3205c2a22ef2af5eed2a26201dd756d43ca
DEVANSH-DOGRA <devansh0422.be21@chitkara.edu.in>
2022-05-28 20:12:55 +0530

    I have appended a code that displays the fibonacci numberts till the number entered

--- largest.cpp
+++ largest.cpp
@@ -9,6 +9,21 @@ cout << "Largest number: " << n1;
 else if ((n2 >= n1) && (n2 >= n3))
 cout << "Largest number: " << n2;
 else
-cout << "Largest number: " << n3;
+cout << "Largest number: " << n3<<endl;
+
+// I AM DEVANSH I AM APPENDING A NEW PROGRAM WHICH PRINTS THE FIBONACCI MEMBERS TILL THE NUMBER ENTERED!!!!
+
+int none=0,nto=1,nt,n;
+cout<<"Enter the no. till which fibonacci series is to be printed"<<endl;
+cin>>n;
+for(int i =0;i<n;i++)
+{
+cout<<n1<<"\n";
+nt=none+nto;
+none=nto;
+nto=nt;
+
+}
 return 0;
 }
+
